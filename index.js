b7e9a10585c7f7dba64e5707a92b98bd749d5a66
Sandipon-Biswas <sandipon2971@gmail.com>
2025-07-26 03:20:36 +0600

    sdfsdf

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://library-management-system-frontend-mu.vercel.app"
+  origin: [
+    "http://localhost:5173",
+    "https://library-management-system-frontend-mu.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
 }));
 
 
