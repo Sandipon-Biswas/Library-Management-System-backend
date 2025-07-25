366b0cfbe062d22dda3ea9be4b8502be9ab48d00
Sandipon-Biswas <sandipon2971@gmail.com>
2025-07-26 02:30:43 +0600

    sfsf

--- database/db.js
+++ database/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-mongoose.set('bufferCommands', false);
+
 const connectToMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    
+      serverSelectionTimeoutMS: 10000, // 10 sec timeout
+      bufferCommands: false, // <-- এই লাইন add করা হয়েছে
     });
     console.log("✅ MongoDB Connected");
   } catch (err) {
