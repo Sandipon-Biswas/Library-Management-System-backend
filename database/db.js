6cdb576252356130a6afd9845325185a11555103
Sandipon-Biswas <sandipon2971@gmail.com>
2025-07-26 01:25:53 +0600

    sdfsdf

--- database/db.js
+++ database/db.js
@@ -1,11 +1,16 @@
-import { connect } from "mongoose"
+import mongoose from "mongoose";
 
-const connectToMongo= async()=>{
-    try {
-        await connect(process.env.MONGO_URI)
-        console.log("database connected")
-    } catch (error) {
-        console.log(error)
-    }
-}
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10 sec timeou
+    });
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Failed", err.message);
+  }
+};
+
 export default connectToMongo;
